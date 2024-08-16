3fce8252fe4840644fa1d3587b34cf6de35c9aba
Oren <orenz0@protonmail.com>
2024-08-17 00:28:23 +0300

    moving the getters to a separate file
    we will need getNip04 and getSigner later
    in NostrConnectBroker, and I don't want to
    have circular imports.

---
+++ src/engine/utils/getters.ts
@@ -0,0 +1,18 @@
+import {memoize} from "src/util/misc"
+import {Nip04} from "./nip04"
+import {Nip44} from "./nip44"
+import {Nip59} from "./nip59"
+import {Signer} from "./signer"
+import {Connect} from "./connect"
+
+export const getConnect = memoize(session => new Connect(session))
+
+export const getSigner = memoize(session => new Signer(session, getConnect(session)))
+
+export const getNip44 = memoize(session => new Nip44(session, getConnect(session)))
+
+export const getNip04 = memoize(session => new Nip04(session, getConnect(session)))
+
+export const getNip59 = memoize(
+  session => new Nip59(session, getNip04(session), getNip44(session), getSigner(session)),
+)
